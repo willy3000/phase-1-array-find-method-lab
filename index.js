23f8bb162e4a322116231fe70c21d3405ab4cfcb
Jillo Wario <willywario0@gmail.com>
2023-12-19 21:20:15 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+
+const superbowlWin = (record) => {
+  const foundObject = record.find((obj) => {
+    return obj.result === "W";
+  });
+  if (foundObject) {
+    return foundObject.year;
+  } else {
+    return undefined;
+  }
+};
